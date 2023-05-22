0bb5d8794f7908f02122adefc44a7f99d9fd47c0
Statick <dsaavedra88@gmail.com>
2023-05-22 17:40:09 -0500

    update javascript

---
+++ Parte Teórica/9-conditionals.js
@@ -0,0 +1,82 @@
+// Esto nos ayudará par
+
+var esUsuario = true; 
+
+if(esUsuario) {  // con
+    console.log('Tiene 
+}
+
+// Presentamos el else 
+
+var esUsuario = false; 
+
+if(esUsuario) {
+    console.log('Tiene 
+} else {
+    console.log('Tienes
+}
+
+// Ejemplo de cómo harí
+
+var edad = 18; 
+var accion; 
+
+if(edad >= 18) {
+    accion = 'Puede vot
+} else { // call back 
+    accion = 'No puede 
+}
+
+console.log(accion);
+
+// Ejemplo de cómo util
+
+var edad = 18; 
+var accion;  
+
+if(edad === 18) {
+    accion = 'Puede vot
+} else if(edad > 18) { 
+    accion = 'Puede vot
+} else { // call back 
+    accion = 'Aun no pu
+}
+
+console.log(accion);
+
+
+
+/* ====================
+
+// Operador ternario 
+
+var numero = 1; 
+var resultado; 
+
+if(numero === 1) {
+    resultado = 'Sí son
+} else {
+    resultado = 'No soy
+}
+
+condition ? true : fals
+
+var numero = 1; 
+var resultado = numero 
+
+console.log(resultado);
+
+
+// ====================
+
+
+// Ejercicio, reaizar u
+
+/*  
+
+Variables piedra, papel
+una funcion que utilice
+
+y que la funcion al fin
+
+*/ 
